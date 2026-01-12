f84b2aaad9c52d0aac32568ee9d76365f16421f5
PwiseV <21520392@gm.uit.edu.vn>
2026-01-13 01:23:11 +0700

    server.js sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { connectDB } from './libs/db.js';
 import "./config/cloudinary.js";
 import authRoute from './routes/auth.route.js'
 import adminRoute from "./routes/admin.route.js";
+import adminOrderRoute from "./routes/adminOrder.route.js";
 import userRoute from "./routes/user.route.js";
 import productRoute from "./routes/product.route.js"
 import postRoute from "./routes/post.route.js"
@@ -41,7 +42,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", protectedRoute, userRoute, cartRoute, orderRoute);
 
 // admin routes (CẦN đăng nhập + quyền admin)
-app.use("/api/admin", protectedRoute, adminOnly, adminRoute, productRoute, postRoute);
+app.use("/api/admin", protectedRoute, adminOnly, adminRoute, productRoute, postRoute, adminOrderRoute);
 
 
 
