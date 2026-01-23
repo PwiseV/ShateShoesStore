8f90814dec706fe4f39419c2b86d79b880f7bbbd
longnguyenha050 <22520965@gm.uit.edu.vn>
2026-01-23 17:27:43 +0700

    implement CRUD for review management

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -1,4 +1,6 @@
 
+import * as reviewService from "../services/review.service.js";
+
 import Review from "../models/Review.js";
 import OrderItem from "../models/OrderItem.js";
 import Product from "../models/Product.js";
@@ -120,5 +122,54 @@ export const getReviewsByProduct = async (req, res) => {
     } catch (error) {
         console.error("Error getReviewsByProduct:", error);
         res.status(500).json({ message: "Lỗi Server" });
+    }
+};
+
+export const getAllReviews = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const status = req.query.status;
+        const rating =  req.query.rating;
+        const { formattedReviews, total } = await reviewService.getAllReviewsService(page, limit, status, rating);
+
+        return res.status(200).json({
+            success: true,
+            message: "Lấy danh sách đánh giá thành công",
+            data: formattedReviews,
+            pagination: {
+                total,
+                page,
+                limit,
+                totalPages: Math.ceil(total / limit)
+            }
+        });
+
+    } catch (error) {
+        console.error("Lỗi trong getAllReviews:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Đã xảy ra lỗi khi lấy danh sách đánh giá",
+            error: error.message
+        });
+    }
+};
+
+export const updateReviewStatus = async (req, res) => {
+    try {
+        const { reviewId } = req.params;
+        const { status } = req.body;
+
+        if (!["active", "hidden"].includes(status)) {
+            return res.status(400).json({ success: false, message: "Trạng thái không hợp lệ" });
+        }
+
+        const data = await reviewService.updateReviewStatusService(reviewId, status);
+        
+        return data 
+            ? res.status(200).json({ success: true, message: "Cập nhật thành công", data })
+            : res.status(404).json({ success: false, message: "Không tìm thấy đánh giá" });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Lỗi hệ thống", error: error.message });
     }
 };
